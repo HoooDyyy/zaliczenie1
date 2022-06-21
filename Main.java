478908812a8feef57262fa0639083962472a10ad
HoooDyyy <101186790+HoooDyyy@users.noreply.github.com>
2022-06-21 10:46:20 +0000

    na 4 elementy

--- Main.java
+++ Main.java
@@ -7,9 +7,10 @@ public class Main {
       objArray.add(0,"element");
       objArray.add(1,"element2");
       objArray.add(2,"element3");
+      objArray.add(3,"element4");
      
       System.out.println("Przed usunieciem"+objArray);
-      objArray.remove(1);
+      objArray.remove(2);
       objArray.remove("0th element");
       System.out.println("Po usunieciu"+objArray);
    }
